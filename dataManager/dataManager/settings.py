d75c3acb67ef9524376eebfb6ef1d42214ce732e
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-12-12 23:21:18 +0500

    db connection setup

--- dataManager/dataManager/settings.py
+++ dataManager/dataManager/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'dataManager.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'stock_data',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'abc123',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
+
 }
 
 
