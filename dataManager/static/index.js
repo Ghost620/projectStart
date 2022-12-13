2714f40aa0f0505f28eb8e4ce76886dc13286999
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-12-13 22:19:24 +0500

    js linked

---
+++ dataManager/static/index.js
@@ -0,0 +1,6 @@
+const submit = () => {
+    var selected_data_source = document.getElementById("data_source")
+    var selected_activity = document.getElementById("activity")
+
+    alert(selected_data_source.value + ', ' + selected_activity.value)
+}
