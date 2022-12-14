19bcf95070477774d0e398cb5553eda4b16a8f6c
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-12-14 19:25:48 +0500

    meta tables page added

--- dataManager/dataManager/urls.py
+++ dataManager/dataManager/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, show_meta_table
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('/admin', admin.site.urls),
+    path('', index, name='index'),
+    path('meta/', show_meta_table, name='show_meta_table')
 ]
