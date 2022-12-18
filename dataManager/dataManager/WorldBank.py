36cfb75144da72cc38eb488db6596bd45e1e87be
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-12-18 20:44:43 +0500

    all tables rendered

--- dataManager/dataManager/WorldBank.py
+++ dataManager/dataManager/WorldBank.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-import os
+import os, json
 import pandas as pd
 
 load_dotenv()
@@ -10,20 +10,31 @@ password=os.getenv('LOCAL_DB_PASSWORD')
 port=int(os.getenv('DB_PORT'))
 host=os.getenv('DB_HOST')
 
+def dict2htmltable(data):
+    html = '<thead class="sticky top-0 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">' + '<tr>' + ''.join('<th scope="col" class="py-3 px-6 text-center">' + x + '</th>' for x in data[0].keys()) + '</tr>' + '<tbody>'
+    
+    for d in data:
+        html += '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">' + ''.join('<td class="py-4 px-6 font-medium text-white text-center">' + f'{x}' + '</td>' for x in d.values()) + '</tr>'
+    return '<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">' + html + '</tbody>' + '</table>'
+
 engine = create_engine(f'postgresql://{user}:{password}@{host}/{database}')
 
-all_tables_query = '''
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema='public'
-    AND table_type='BASE TABLE';
-'''
-
-df = pd.read_sql_query(all_tables_query, engine)
-
 def fetchWorldBankDB():
-    global engine, df
+    global engine
+
+    all_tables_query = '''
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema='public'
+        AND table_type='BASE TABLE';
+    '''
+
+    df = pd.read_sql_query(all_tables_query, engine)
     world_bank_tables = [i for i in df['table_name'] if 'world_bank' in i]
+
     queries = [f'''SELECT * FROM {table};''' for table in world_bank_tables]
-    all_dbs = [pd.read_sql_query(quer, engine) for quer in queries]
-    return all_dbs
+    all_tables = [pd.read_sql_query(quer, engine, index_col='id') for quer in queries]
+    data = [json.loads(df.reset_index().to_json(orient ='records')) for df in all_tables]
+    tables = [dict2htmltable(df) for df in data]
+
+    return tables
--- dataManager/dataManager/views.py
+++ dataManager/dataManager/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-import json
 from .WorldBank import fetchWorldBankDB
 
 def index(request):
@@ -9,8 +8,5 @@ def show_meta_table(request):
     if request.method == 'POST':
 
         all_tables = fetchWorldBankDB()
-        data = [json.loads(df.reset_index().to_json(orient ='records')) for df in all_tables]
-
-        context = {'table_name': request.POST['data_source'], 'data': data }
-    
+        context = {'table_name': request.POST['data_source'], 'data': all_tables }
         return render(request, 'meta.html', context)
